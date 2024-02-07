b2efa4f0bf3f59e9ca7f41de1be1bc7a9f5e128a
zonah <124108725+kimzonah@users.noreply.github.com>
2024-02-08 02:03:11 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 159 ms, Memory: 28,320 KB -BaekjoonHub

---
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
@@ -0,0 +1,44 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args){
+		
+		Scanner sc = new Scanner(System.in);
+		
+		for(int t=1; t<11; t++) {
+			
+			int N = sc.nextInt();
+			Queue<Integer> numQue = new LinkedList<>();
+			
+			for(int i=0; i<8; i++) {
+				numQue.add(sc.nextInt());
+			}
+			
+			loop : while(true) {
+				for(int d=1; d<=5; d++) {
+					int lastNum = numQue.poll()-d;
+					// 뒤에 올 숫자가 0이하면 0 넣고 탈출
+					if(lastNum <= 0) {
+						numQue.add(0);
+						break loop;
+					}// 아니면 그냥 값 삽입
+					else
+						numQue.add(lastNum);
+				}
+			}
+			
+			
+			System.out.print("#"+t);
+			for(int i=0; i<8; i++) {
+				System.out.print(" " + numQue.poll());
+			}
+			System.out.println();
+			
+			
+			
+		}
+	}
+
+}
