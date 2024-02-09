fd1b1d12994ae960507ddf6e88f918a7b38a1801
[seonha] <[ksh219805@gmail.com]>
2024-02-09 15:12:08 +0900

    [Silver IV] Title:스위치 켜고 끄기, Time:148ms, Memory:14612KB - BackjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -1,97 +1,87 @@
-// [백준] 1244. 스위치 켜고 끄기 (Java)
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
- 
+
 public class Main {
-	static int num; // 스위치 개수
-	static int[] state; // 스위치 상태
-	static int student; // 학생 수
-	
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-  
-        // 스위치 개수 입력
-        num = Integer.parseInt(br.readLine());
-        
-        // 스위치 상태 입력
-        state = new int[num + 1];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 1; i < state.length; i++) {
-        	state[i] = Integer.parseInt(st.nextToken());
-        }
-        
-        // 학생수 입력
-        student = Integer.parseInt(br.readLine());
-        
-        // 학생 스위치 처리
-        for(int i = 0; i < student; i++) {
-        	st = new StringTokenizer(br.readLine());
-        	
-        	int gender = Integer.parseInt(st.nextToken());
-        	
-        	// 남자
-        	if(gender == 1) {
-        		man(Integer.parseInt(st.nextToken()));
-        	}
-        	// 여자
-        	else if(gender == 2) {
-        		women(Integer.parseInt(st.nextToken()));
-        	}
-        }
-        
-        for(int i = 1; i < state.length; i++) {
-        	System.out.print(state[i] + " ");
-        	if(i % 20 == 0) {
-        		System.out.println();
-        	}
-        }
-        br.close();
-    }
-    
-    public static void man(int x) {
-    	int cur = x;
-    	// 배수 번호 스위치 상태 반대로 변경
-    	while(cur < state.length) {
-    		if(state[cur] == 0) {
-    			state[cur] = 1;
-    		}
-    		else if(state[cur] == 1) {
-    			state[cur] = 0;
-    		}
-    		cur += x;
-    	}
-    }
-    
-    public static void women(int x) { 	
-    	// 현재 위치 스위치 상태 반대로 변경
-    	if(state[x] == 0) {
-			state[x] = 1;
+
+	static int switchSize; // 스위치 개수
+	static int[] switchArr; // 스위치 상태 배열
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		// 스위치 개수 입력
+		switchSize = Integer.parseInt(br.readLine());
+
+		// 스위치 개수+1개 크기의 스위치 배열 생성. 인덱스 번호를 스위치 번호랑 같게 하기 위해
+		switchArr = new int[switchSize + 1];
+
+		// 스위치 상태 입력 후 배열에 인덱스 1부터 넣어주기
+		st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= switchSize; i++) {
+			switchArr[i] = Integer.parseInt(st.nextToken());
 		}
-		else if(state[x] == 1) {
-			state[x] = 0;
+
+		// 학생 수 입력
+		int stuNum = Integer.parseInt(br.readLine());
+
+		// 학생 수 만큼 성별, 받은 수 입력 받으면서 조건에 맞게 스위치 켜고 끄기 반복
+		for (int i = 0; i < stuNum; i++) {
+			st = new StringTokenizer(br.readLine());
+
+			int gender = Integer.parseInt(st.nextToken());
+			int num = Integer.parseInt(st.nextToken());
+
+			if (gender == 1)
+				Male(num);
+
+			else
+				Female(num);
+
 		}
-    	
-    	// 대칭 스위치 반대로 변경
-    	int count = 1;
-    	while((x - count) > 0 && (x + count) < state.length) {
-    		if(state[x + count] == state[x - count]) {
-    			
-    			if(state[x + count] == 0) {
-    				state[x + count] = 1;
-    				state[x - count] = 1;
-    			}
-    			else if(state[x + count] == 1) {
-    				state[x + count] = 0;
-    				state[x - count] = 0;
-    			}
-    			count++;
-    			
-    		}
-    		else {
-    			break;
-    		}
-    	}
-    }
+
+		for (int i = 1; i <= switchSize; i++) {
+			System.out.print(switchArr[i] + " ");
+			if (i % 20 == 0)
+				System.out.println();
+		}
+
+	}
+
+	// 남자일 때 수행할 메서드
+	public static void Male(int num) {
+		int n = num; // n 초기값을 num으로
+		// n이 스위치 마지막 번호보다 작거나 같아질 때까지
+		while (n <= switchSize) {
+			Toggle(n); // n 번째 스위치 토글
+			n += num; // n에 받은 수만큼 더해서 갱신(배수)
+		}
+	}
+
+	// 여자일 때 수행할 메서드
+	public static void Female(int num) {
+		// 일단 받은수번째 스위치는 무조건 토글
+		Toggle(num);
+		int d = 1;
+		// 받은 수번째 스위치에서 양옆 한칸씩 비교하면서 토글
+		// 양옆으로 이동한 인덱스가 1~스위치개수 사이어야 함
+		while (num - d >= 1 && num + d <= switchSize) {
+			// 만약에 양옆 한칸이 같다면 각각 토글해주고 한칸씩 더 이동해서 다시 비교
+			if (switchArr[num - d] == switchArr[num + d]) {
+				Toggle(num - d);
+				Toggle(num + d);
+				d += 1;
+			} else
+				break;
+		}
+	}
+
+	public static void Toggle(int n) {
+		if (switchArr[n] == 0)
+			switchArr[n] = 1;
+		else
+			switchArr[n] = 0;
+	}
+
 }
