aba7f0a02d96db2a52fbcbf768b5895dfffb8da5
zonah <124108725+kimzonah@users.noreply.github.com>
2024-04-05 11:30:54 +0900

    [Gold IV] Title: 불, Time: 792 ms, Memory: 140024 KB -BaekjoonHub

--- 백준/Gold/5427. 불/불.java
+++ 백준/Gold/5427. 불/불.java
@@ -9,13 +9,11 @@ public class Main {
 	
 	static int W, H;
 	static char[][] map;
-//	static boolean[][] visitedFire;
 	static boolean[][] visitedSang;
 	static Queue<int[]> queFire;
 	static Queue<int[]> queSang;
 	static int[] dr = {-1, 1, 0, 0};
 	static int[] dc = {0, 0, -1, 1};
-//	static int[] last = new int[2];
 	static int time;
 	static boolean exit;
 	
@@ -33,14 +31,8 @@ public class Main {
 			W = Integer.parseInt(st.nextToken());
 			H = Integer.parseInt(st.nextToken());
 			
-//			int[] sFire = new int[2];
-//			int[] sSang = new int[2];
-			
 			map = new char[H][W];
-//			visitedFire = new boolean[H][W];
 			visitedSang = new boolean[H][W];
-			
-//			int exitCnt = 0;
 			
 			queFire = new LinkedList<>();
 			queSang = new LinkedList<>();
@@ -50,34 +42,16 @@ public class Main {
 				for(int c=0; c<W; c++) {
 					map[r][c] = tmp.charAt(c);
 					if(map[r][c] == '@') { // 상근이 위치면 상근이 큐에 넣고 방문처리
-//						sSang[0] = r;
-//						sSang[1] = c;
 						queSang.add(new int[] {r, c});
 						visitedSang[r][c] = true;
 					}
 					
 					if(map[r][c] == '*') { // 불 위치면 불 큐에 넣기
-//						sFire[0] = r;
-//						sFire[1] = c;
 						queFire.add(new int[] {r, c});
 					}
 					
-//					if((r==0 || r==H-1 || c==0 || c==W-1) && map[r][c]=='.') {
-//						exitCnt++;
-//					}
 				}
 			}
-			
-//			if(exitCnt == 0) {
-//				System.out.println("IMPOSSIBLE");
-//				continue;
-//			}
-			
-//			queFire.add(sFire);
-//			visitedFire[sFire[0]][sFire[1]] = true;
-//			
-//			queSang.add(sSang);
-//			visitedSang[sSang[0]][sSang[1]] = true;
 			
 			time = 0;
 			exit = false;
@@ -93,19 +67,17 @@ public class Main {
 	
 	static void bfs() {
 		
-//		int fSize = 1;
-//		int sSize = 1; // 상근이 자리는 무조건 1로 시작하니까
+		int sSize = 0;
+		int fSize = 0;
 		
 		bfs :while(!queSang.isEmpty()) {
 			
 			// 일단 현재 상근이 자리 중에 출구가 있으면
-			int sSize = queSang.size();
+			sSize = queSang.size();
 			for(int i=0; i<sSize; i++) {
 				int[] now = queSang.poll();
 				int r = now[0];
 				int c = now[1];
-				
-//				System.out.println("r : "+r + ", c : "+c);
 				
 				if(r==0 || r==H-1 || c==0 || c==W-1) {
 					exit = true;
@@ -119,7 +91,7 @@ public class Main {
 			time++;
 			
 			// 불 먼저 이동시키기
-			int fSize = queFire.size(); // 현재 시간에 만들어진 불만큼만 체크
+			fSize = queFire.size(); // 현재 시간에 만들어진 불만큼만 체크
 			for(int i=0; i<fSize; i++) {
 				int[] nowF = queFire.poll();
 				int r = nowF[0];
@@ -140,13 +112,6 @@ public class Main {
 					}
 				}
 			}
-			
-//			for(int r=0; r<H; r++) {
-//				for(int c=0; c<W; c++) {
-//					System.out.print(map[r][c]+" ");
-//				}
-//				System.out.println();
-//			}
 			
 			// 상근이 이동시키기
 			sSize = queSang.size();
@@ -170,14 +135,6 @@ public class Main {
 						queSang.add(new int[] {nr, nc});
 						
 						
-//						if(nr==0 || nr==H-1 || nc==0 || nc==W-1) {
-//							exit = true;
-//							break bfs;
-//						}
-//						else {
-//							visitedSang[nr][nc] = true;
-//							queSang.add(new int[] {nr, nc});
-//						}
 					}
 				}
 			}
